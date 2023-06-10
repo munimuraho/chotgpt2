a9ef9563ca8459a5561e5cdf9d5cefcc369e15e1
munimuraho <munimuraho@gmail.com>
2023-06-11 06:46:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-        model={selected_item},
+        model="gpt-3.5-turbo",
         messages=messages,
         temperature= 1.12
     )  
