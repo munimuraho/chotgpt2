555c2dae0910cc3cb1ab3d26357dbbfe068067ad
munimuraho <munimuraho@gmail.com>
2023-06-12 06:41:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os
-
 import streamlit as st
-from streamlit_chat import message
 
 import openai
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
@@ -15,11 +13,6 @@ from langchain.prompts.chat import (
 )
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
-from langchain.callbacks.base import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-
-# Does it work?
-from langchain.callbacks.streamlit import StreamlitCallbackHandler
 
 system_message = """
 あなたは研究アシスタントです。ユーザは研究者で、あなたに研究に関する質問を投げかけます。
@@ -31,17 +24,8 @@ prompt = ChatPromptTemplate.from_messages([
 ])
 
 @st.cache_resource
-def load_conversation():
-  llm = ChatOpenAI(
-    streaming=True,
-    callback_manager=CallbackManager([
-      StreamlitCallbackHandler(),
-      StreamingStdOutCallbackHandler()
-    ]),
-    verbose=True,
-    temperature=0,
-    max_tokens=1024
-  )
+def get_conversation():
+  llm = ChatOpenAI(temperature=0)
   memory = ConversationBufferMemory(return_messages=True)
   conversation = ConversationChain(
     memory=memory,
@@ -51,24 +35,10 @@ def load_conversation():
   return conversation
 
 st.title("研究アシスタント")
-
-if "generated" not in st.session_state:
-    st.session_state.generated = []
-if "past" not in st.session_state:
-    st.session_state.past = []
-
 with st.form("研究アシスタントに質問する"):
   user_message = st.text_area("質問を入力してください")
-
   submitted = st.form_submit_button("質問する")
   if submitted:
-    conversation = load_conversation()
+    conversation = get_conversation()
     answer = conversation.predict(input=user_message)
-
-    st.session_state.past.append(user_message)
-    st.session_state.generated.append(answer)
-
-    if st.session_state["generated"]:
-      for i in range(len(st.session_state.generated) - 1, -1, -1):
-        message(st.session_state.generated[i], key=str(i))
-        message(st.session_state.past[i], is_user=True, key=str(i) + "_user")
+    st.write(answer)
