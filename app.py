fa90739ec66fac81a26b794d0c68d1092ba3aff2
munimuraho <munimuraho@gmail.com>
2023-07-17 06:27:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import google.auth
-from google.auth.transport.requests import Request
-from google.oauth2 import id_token
-from streamlit_chat import message
 import openai
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
@@ -14,50 +10,8 @@ if "messages" not in st.session_state:
         {"role": "system", "content": st.secrets.AppSettings.chatbot_setting}
     ]
 
-# 認証チェック関数
-def authenticate():
-    client_id = "st.secrets.AuthSettings.googleclientid"  # Google OAuth2.0のクライアントIDを指定してください
-    st.write("認証中...")
-    credentials, _ = google.auth.default()
-
-    if credentials and credentials.valid:
-        id_info = id_token.verify_oauth2_token(credentials.token, Request(), client_id)
-        if id_info:
-            st.write("認証に成功しました！")
-            return True
-    else:
-        st.write("認証に失敗しました。再試行してください。")
-
-    st.write("OAuth認証を開始します...")
-    auth_url, _ = credentials.authorization_url("https://accounts.google.com/o/oauth2/auth", access_type="offline")
-
-    if "code" not in st.session_state:
-        st.write(f"[Googleで認証する]({auth_url})")
-    else:
-        try:
-            credentials.fetch_token(
-                "https://accounts.google.com/o/oauth2/token",
-                authorization_response=st.session_state["code"],
-                client_secret="st.secrets.AuthSettings.googleclientsecret"  # Google OAuth2.0のクライアントシークレットを指定してください
-            )
-
-            st.session_state.pop("code")  # クリア
-
-            id_info = id_token.verify_oauth2_token(credentials.token, Request(), client_id)
-            if id_info:
-                st.write("認証に成功しました！")
-                return True
-        except Exception as e:
-            st.write(f"認証エラー: {e}")
-
-    return False
-
-
 # チャットボットとやりとりする関数
 def communicate():
-    if not authenticate():
-        return
-
     messages = st.session_state["messages"]
 
     user_message = {"role": "user", "content": st.session_state["user_input"]}
@@ -78,6 +32,15 @@ def communicate():
 # ユーザーインターフェイスの構築
 st.title("I'm ChotGPT!!!")
 st.write("ChatGPT APIを使ったチャットボットです。")
+
+if st.button("Googleでログイン"):
+    user = st.oauth_login(st.secrets.AuthSettings.googleclient, "https://www.googleapis.com/auth/userinfo.profile")
+    if user:
+        st.write("認証に成功しました！")
+        st.write(f"ユーザー名: {user['name']}")
+        st.write(f"メールアドレス: {user['email']}")
+    else:
+        st.write("認証に失敗しました。再試行してください。")
 
 user_input = st.text_input("できるだけ頑張ります。以下にテキストをどうそ。", key="user_input", on_change=communicate)
 
